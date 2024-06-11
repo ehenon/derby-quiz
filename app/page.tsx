a2f748f67e8d6c2dc5a6dc69a1e2c320fc0b6847
ehenon <emilienhenon@gmail.com>
2024-06-11 19:40:11 +0200

    feat(#8): get a multiplayer game room system 🎮

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,35 @@
 'use client';
 
-import { useState } from 'react';
+import Game from '@components/Game';
+import avatars from '@utils/avatars';
+import { insertCoin } from 'playroomkit';
+import { useEffect, useState } from 'react';
 
-const Page = () => {
-  const [question, setQuestion] = useState<string>('');
+const Lobby = () => {
+  const [isJoined, setIsJoined] = useState<boolean>(false);
 
-  const getRandomQuestion = async () => {
-    try {
-      const res = await fetch('/api/random-question', {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        cache: 'no-store',
-      });
-
-      if (!res.ok) {
-        throw new Error(res.status.toString());
+  useEffect(() => {
+    const joinPlayRoom = async () => {
+      try {
+        await insertCoin({
+          gameId: process.env.PLAYROOM_GAME_ID,
+          avatars,
+          reconnectGracePeriod: 5000,
+          maxPlayersPerRoom: 4,
+        });
+        setIsJoined(true);
+      } catch (err) {
+        console.log('Error occurred in joinPlayRoom(): ', err);
       }
-
-      const { label } = await res.json();
-      setQuestion(label);
-    } catch (err) {
-      console.log('Une erreur est survenue');
-    }
-  };
+    };
+    joinPlayRoom();
+  });
 
   return (
-    <div className='w-full min-h-screen flex flex-col items-center justify-center'>
-      <button onClick={getRandomQuestion} className="group relative h-12 w-96 overflow-hidden rounded-lg bg-white text-lg shadow">
-        <div className="absolute inset-0 w-3 bg-blue-400 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
-        <span className="relative text-black group-hover:text-white">Donne-moi une question !</span>
-      </button>
-      <p className='w-2/4 ml-12 mt-12 mr-12 text-justify'>{question}</p>
-    </div>
+    <main className="text-center">
+      {isJoined && <Game />}
+    </main>
   );
 };
 
-export default Page;
+export default Lobby;
---
+++ utils/avatars.ts
@@ -0,0 +1,9 @@
+const avatars = [
+  '/avatars/baseball_male_player.svg',
+  '/avatars/female_jockey.svg',
+  '/avatars/football_female_player.svg',
+  '/avatars/football_male_referee.svg',
+  '/avatars/hockey_male_player.svg',
+];
+
+export default avatars;
